ae6cd5b28972794619a6ae313b3b22fd62c37227
kankanit saengthao <nut57130@gmail.com>
2022-08-06 16:10:36 +0700

    กลับบ้านละ

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -16,42 +16,127 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       drawer: const Drawer(),
       body: Container(
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border(
-              bottom:BorderSide(
-                color: Colors.black12,
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.black12,
+                  ),
+                ),
+              ),
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Icon(Icons.home), Text("Home")],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Icon(Icons.newspaper), Text("ข่าวสาร")],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Icon(Icons.info), Text("เกียบกับ")],
+                  ),
+                ],
               ),
             ),
-          ),
-          height: 50,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+            Container(
+              height: 280,
+              color: Colors.blue,
+              child: Row(
+                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.home), 
-                  Text("Home")
+                  Container(
+                     padding: EdgeInsets.only(top: 10, right: 10, left: 10),
+                    width: 200,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                           width: double.infinity,
+                          margin: EdgeInsets.only(bottom: 5),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black54,
+                            ),
+                          ),
+                          child: Text("Strawberry Pavlova"),
+                        ),
+                        Container(
+                           width: double.infinity,
+                          margin: EdgeInsets.only(bottom: 5),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black54,
+                            ),
+                          ),
+                          child: Text("Pavlova is a meringue-basd \n dessert named after  the Russian \n ballerine Anna Pavlova \n Pavlova featues a crisp srust and \n soft, light inside, topped with fruit\n andwhipped cream"),
+                        ),
+                        Container(
+                           width: double.infinity,
+                          margin: EdgeInsets.only(bottom: 5),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black54,
+                            ),
+                          ),
+                          child: Row(
+                            children:[
+                              Icon(Icons.star,size:10),
+                              Icon(Icons.star,size:10),
+                              Icon(Icons.star,size:10),
+                              Icon(Icons.star,size:10),
+                              Icon(Icons.star,size:10),
+                              Text("170 Reviews")
+                            ]
+                          ),
+                        ),
+                        Container(
+                           decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black54,
+                            ),
+                          ),
+                          child: Row(
+                            children: [
+                              Column(
+                                children: [
+                                  Icon(Icons.ac_unit),
+                                  Text("Prep :"),
+                                  Text("25 mins"),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Icon(Icons.lock_clock),
+                                  Text("Cook"),
+                                  Text("1 hrs"),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Icon(Icons.ac_unit),
+                                  Text("Feed :"),
+                                  Text("4-6"),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(),
                 ],
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.newspaper), 
-                  Text("ข่าวสาร")
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.info), 
-                  Text("เกียบกับ")
-                ],
-              ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(items: [
