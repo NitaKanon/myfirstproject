d0443c62b0a44f8d677f789b1ebc51b5eea76815
kankanit saengthao <nut57130@gmail.com>
2022-08-06 14:47:54 +0700

    เสร็จสึ้นที่อบรมวันเสาร์

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -15,14 +15,36 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("HomeScreen"),
       ),
       drawer: Drawer(),
-      body: const Center(
-        child: Text("Hello from HomeScreen"),
+      body: Container(
+        color: Colors.blue[400],
+        margin: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            children: [
+              Text("Hello World"),
+              ElevatedButton(
+                onPressed: () => {},
+                child: Text("Press me"),
+              )
+              
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "หน้าหลัก"),
-        BottomNavigationBarItem(icon: Icon(Icons.article), label: "ข่าวสาร"),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "หน้าหลัก",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.article),
+          label: "ข่าวสาร",
+        ),
         //BottomNavigationBarItem(icon: Icon(Icons.home), label: "บทความ"),
-        BottomNavigationBarItem(icon: Icon(Icons.info), label: "เกี่ยวกับ"),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.info),
+          label: "เกี่ยวกับ",
+        ),
       ]),
     );
   }
